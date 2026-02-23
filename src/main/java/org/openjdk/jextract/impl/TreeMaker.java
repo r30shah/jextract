e475a3eea6d5e818cd01b44391b0e9be83017ee6
Rahil Shah <rahil@ca.ibm.com>
2026-02-23 16:37:33 -0500

    Fix Debug Print

--- src/main/java/org/openjdk/jextract/impl/TreeMaker.java
+++ src/main/java/org/openjdk/jextract/impl/TreeMaker.java
@@ -311,7 +311,7 @@ class TreeMaker {
             pendingBitfieldsPos.set(null);
         }
 
-        System.out.println("RAHILDEBUG: TreeMaker.java:314 - Walked through all members of Struct/Union for "+recordCursor.displayName()+" spelling ="+fc.spelling());
+        System.out.println("RAHILDEBUG: TreeMaker.java:314 - Walked through all members of Struct/Union for "+recordCursor.displayName()+" spelling ="+recordCursor.spelling());
         Scoped structOrUnionDecl = recordCursor.kind() == CursorKind.StructDecl ?
                 Declaration.struct(CursorPosition.of(recordCursor), recordCursor.spelling(),
                         pendingFields.toArray(new Declaration[0])) :
